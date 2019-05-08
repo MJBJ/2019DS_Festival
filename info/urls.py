4fe7449446d218c5dc1fec50a7babaa09952542c
minjuhui <mzuhui001@gmail.com>
2019-05-09 02:51:09 +0900

    이전

--- info/urls.py
+++ info/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.main),
-    # path('map', views.map),
-    # path('notice', views.notice),
-    # path('timetable', views.timetable),
-    # path('detail', views.detail),
+    path('', views.main),
+    path('map', views.map),
+    path('notice', views.notice),
+    path('timetable', views.timetable),
+    path('detail', views.detail),
     path('',views.dday, name='dday'),
 ]
