11bc1f925609da359fca7ab2845a5d602764d69e
youhyun97 <paris972006@gmail.com>
2019-05-14 05:15:45 +0900

    url 닫기

--- info/urls.py
+++ info/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     # path('notice', views.notice, name='notice'),
     # path('timetable', views.timetable, name='timetable'),
     # path('detail', views.detail, name='detail'),
-    path('dday',views.dday, name='dday'),
+    # path('dday',views.dday, name='dday'),
     # path('develop', views.develop, name='develop'),
     # path('board/', include('board.urls')),
     # path('stamp/', include('stamp.urls')),
+    path('',views.dday, name='dday'),
 ]
