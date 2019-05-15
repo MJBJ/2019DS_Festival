80a834c12cff338be78955c8490fc57126d24945
youhyun97 <paris972006@gmail.com>
2019-05-16 04:58:09 +0900

    unsetting & hosts 변경

--- ds_operator/settings.py
+++ ds_operator/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = False
 # DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['dsfestival2019.herokuapp.com']
 
 
 # Application definition
