2168e36cb810fa5b7d23b64f7c7c61e560f33a44
youhyun97 <paris972006@gmail.com>
2019-05-15 15:52:27 +0900

    tab

--- board/views.py
+++ board/views.py
@@ -6,16 +6,16 @@ from .forms import BoardForm
 from django.http import HttpResponseRedirect
 
 def show(request):
-        boards = Board.objects.order_by('-id')
-        return render(request, 'show.html', {'boards':boards})
+    boards = Board.objects.order_by('-id')
+    return render(request, 'show.html', {'boards':boards})
 
 def photo(request):
-        boards = Board.objects
-	    return render(request, 'photo.html', {'boards':boards})
+    boards = Board.objects 
+	return render(request, 'photo.html', {'boards':boards})
 
 def detail(request,board_id):
-        board_detail = get_object_or_404(Board, pk=board_id)
-        return render(request, 'detail.html', {'board':board_detail})
+    board_detail = get_object_or_404(Board, pk=board_id)
+    return render(request, 'detail.html', {'board':board_detail})
 
 # def create(request): #입력받은 내용을 데이터베이스에 넣어주는 함수
 #     if request.method == 'POST':
@@ -29,23 +29,23 @@ def detail(request,board_id):
 #         # board id는 int형. url은 string. 따라서 문자열로 형변환해줌
 
 def boardpost(request):
-        # 1. 입력된 내용을 처리하는 기능 -> POST    
-        if request.method == 'POST':
-            form = BoardForm(request.POST, request.FILES)
-            if form.is_valid():
-                post = form.save(commit=False) # 저장하지 않고 모델객체를 반환
-                post.updated_at = timezone.now()
-                post.save()
-                return redirect('show')
-        # 2. 빈 페이지를 띄워주는 기능 -> GET
-        else:
-            form = BoardForm()
-            return render(request, 'post.html', {'form':form})
+	# 1. 입력된 내용을 처리하는 기능 -> POST    
+	if request.method == 'POST':
+		form = BoardForm(request.POST, request.FILES)
+		if form.is_valid():
+			post = form.save(commit=False) # 저장하지 않고 모델객체를 반환
+			post.updated_at = timezone.now()
+			post.save()
+			return redirect('show')
+	# 2. 빈 페이지를 띄워주는 기능 -> GET
+	else:
+		form = BoardForm()
+		return render(request, 'post.html', {'form':form})
 
 def delete(request, board_id):
-        board = Board.objects.get(pk=board_id)
-        if board.pwd==request.GET['passwd']:
-                board.delete()
-                return redirect('show')
-        else:
-                return redirect('show')
+	board = Board.objects.get(pk=board_id)
+	if board.pwd==request.GET['passwd']:
+			board.delete()
+			return redirect('show')
+	else:
+			return redirect('show')
