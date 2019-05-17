925a900751f7c2e5c78b27d51e54cf02543c4471
youhyun97 <paris972006@gmail.com>
2019-05-18 00:40:35 +0900

    1차 오픈 직전

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Board(models.Model):
     file = models.FileField(null=True, blank=True)
     Title = models.CharField(max_length=20, blank=True) # 제목
-    Text = models.TextField(max_length=100, blank=True)
+    Text = models.TextField(max_length=200, blank=True)
     updated_at = models.DateTimeField(auto_now=True) # 현재시각
     pwd = models.CharField(max_length=10, null=False, default="0000") # 비밀번호
 
