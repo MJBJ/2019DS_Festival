1239869a438db2750faaec2ac74aee0db8e90c18
youhyun97 <paris972006@gmail.com>
2019-05-18 17:01:47 +0900

    js

--- board/admin.py
+++ board/admin.py
@@ -1,14 +1,5 @@
 from django.contrib import admin
 from .models import Board
-from video_encoding.admin import FormatInline
-from .models import Video
+
 # Register your models here.
 admin.site.register(Board)
-
-# @admin.register(Video)
-#     class VideoAdmin(admin.ModelAdmin):
-#     inlines = (FormatInline,)
-
-#     list_dispaly = ('get_filename', 'width', 'height', 'duration')
-#     fields = ('file', 'width', 'height', 'duration')
-#     readonly_fields = fields
--- board/models.py
+++ board/models.py
@@ -1,7 +1,6 @@
-from django.contrib.contenttypes.fields import GenericRelation
+
 from django.db import models
-from video_encoding.fields import VideoField
-from video_encoding.models import Format
+
 
 class Board(models.Model):
     file = models.FileField(null=True, blank=True)
@@ -15,13 +14,3 @@ class Board(models.Model):
 
     def summary(self):
         return self.Text[:15]
-
-# class Video(models.Model):
-#     width = models.PositiveIntegerField(editable=False, null=True)
-#     height = models.PositiveIntegerField(editable=False, null=True)
-#     duration = models.FloatField(editable=False, null=True)
-
-#     file = VideoField(width_field='width', height_field='height',
-#                         duration_field='duration')
-
-#     format_set = GenericRelation(Format)
