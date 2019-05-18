cd31fc2a7cf6803f0f7cc536225f8425420c393a
youhyun97 <paris972006@gmail.com>
2019-05-18 11:53:58 +0900

    .gitignore media delete

--- board/models.py
+++ board/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Board(models.Model):
-    file = models.FileField()
+    file = models.FileField(null=True, blank=True)
     Title = models.CharField(max_length=20, blank=True) # 제목
     Text = models.TextField(max_length=200, blank=True)
     updated_at = models.DateTimeField(auto_now=True) # 현재시각
