4484e47354266848e2ca8056b6efb7f5027b1b41
youhyun97 <paris972006@gmail.com>
2019-05-18 16:40:19 +0900

    hid.value 사용

--- board/admin.py
+++ board/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Board
+from video_encoding.admin import FormatInline
+from .models import Video
 # Register your models here.
 admin.site.register(Board)
+
+# @admin.register(Video)
+#     class VideoAdmin(admin.ModelAdmin):
+#     inlines = (FormatInline,)
+
+#     list_dispaly = ('get_filename', 'width', 'height', 'duration')
+#     fields = ('file', 'width', 'height', 'duration')
+#     readonly_fields = fields
