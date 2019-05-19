d0abc56654ab41918ec5df732aad3fb66ce2c477
youhyun97 <paris972006@gmail.com>
2019-05-19 23:41:24 +0900

    heroku bucketeerrrr

--- info/urls.py
+++ info/urls.py
@@ -4,14 +4,14 @@ from . import views
 import board.views
 import stamp.views 
 urlpatterns = [
-    # path('main', views.main, name='main'),
-    # path('map', views.map, name='map'),
-    # path('notice', views.notice, name='notice'),
-    # path('timetable', views.timetable, name='timetable'),
-    # path('detail', views.detail, name='detail'),
-    # path('dday',views.dday, name='dday'),
-    # path('develop', views.develop, name='develop'),
-    # path('board/', include('board.urls')),
-    # path('stamp/', include('stamp.urls')),
+    path('main', views.main, name='main'),
+    path('map', views.map, name='map'),
+    path('notice', views.notice, name='notice'),
+    path('timetable', views.timetable, name='timetable'),
+    path('detail', views.detail, name='detail'),
+    path('dday',views.dday, name='dday'),
+    path('develop', views.develop, name='develop'),
+    path('board/', include('board.urls')),
+    path('stamp/', include('stamp.urls')),
     path('',views.dday, name='dday'),
 ]
